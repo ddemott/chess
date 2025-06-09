718c635ea178a563be768961ea71ab7313b7ea38
Dale DeMott <daledemott@gmail.com>
2025-06-09 01:21:07 -0500

    Fix spelling in status messages.

--- src/main/java/com/redsky/project/chess/constants/STATUS.java
+++ src/main/java/com/redsky/project/chess/constants/STATUS.java
@@ -4,9 +4,9 @@ public interface STATUS {
 	public static final String OK = "SUCCESS";
 	public static final String INVALID_MOVE_FOR_PIECE = "ERROR: Move invalid for piece type.";
 	public static final String DESTINATION_NOT_ON_BOARD = "ERROR: Destination not on board.";
-	public static final String STARTING_POSITION_NOT_ON_BOARD = "ERROR: Starting postion not on board.";
+        public static final String STARTING_POSITION_NOT_ON_BOARD = "ERROR: Starting position not on board.";
 	public static final String SPACE_STATUS_OCCUPIED_BY_YOUR_PIECE = "ERROR: Can’t move because a space is occupied by own color piece.";
-	public static final String SPACE_STATUS_OCCUPIED_BY_YOUR_OPPONENTS_PIECE = "ERROR: Space is occuppied by your opponent's piece";
+        public static final String SPACE_STATUS_OCCUPIED_BY_YOUR_OPPONENTS_PIECE = "ERROR: Space is occupied by your opponent's piece";
 	public static final String BLOCKED_BY_YOUR_OWN_PIECE = "ERROR: You cannot move there because you are blocked by your own piece";
 	public static final String BLOCKED_BY_YOUR_OPPONENTS_PIECE = "ERROR: You cannot move there because you are blocked by your opponent's piece";
 	public static final String NO_PIECE_AT = "ERROR: There is no piece to move at ";
